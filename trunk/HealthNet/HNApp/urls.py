9615c2e7d77a074ef0c8cc73f6e57d19528a9ae5
Brandon Bires-Navel <brn5915@rit.edu>
2017-03-05 18:16:01 -0500

    views and stuff

--- trunk/HealthNet/HNApp/urls.py
+++ trunk/HealthNet/HNApp/urls.py
@@ -6,12 +6,15 @@ app_name = 'HNApp'
 urlpatterns = [
     # /
     url(r'^$', views.index, name='index'),
-    # /create
-    url(r'^create/$', views.CreateTool.as_view(success_url="/"), name='create'),
+
+
+    # /accounts...
     # /accounts/patient_signup/
     url(r'^accounts/patient_signup/$', views.register, name="patient_signup"),
-    url(r'^accounts/patient_signup/true$', views.register, name="patient_signup"),
-    # Later on can changed by type of users
+    # /accounts/patient_signup/true/
+    url(r'^accounts/patient_signup/true/$', views.loggedin, name="valid_login"),
+    # /accounts/patient_signup/false
+    url(r'^accounts/patient_signup/false/$', views.invalid_login, name="invalid_login"),
     # /accounts/register_success/
     url(r'^accounts/register_success/$', views.register_success, name="register_success"),
     # /accounts/login/
@@ -24,28 +27,33 @@ urlpatterns = [
     url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
     # /accounts/invalid_login/
     url(r'^accounts/invalid_login/$', views.invalid_login, name='invalid_login'),
-    # /profile
-    # url(r'^(?P<name>/profile)/$', views.profile, name=''),
+    # /accounts/profile/
     url(r'^accounts/profile/$', views.profile, name='profile'),
+
+    # appointments...
     # /create-appointment
     url(r'^create_appointment/$', views.CreateAppointmentView.as_view(), name='create_appointment'),
     # time slot is taken
     url(r'^create_appointment/time_taken$', views.time_taken, name='time_taken'),
+    # /edit_appointment
+    url(r'^edit_appointment//(?P<pk>[0-9]+)/$', views.EditAppointment.as_view(), name='edit_appointment'),
+    # /edit_appointment/time_taken
+    url(r'^edit_appointment/time_taken/$', views.time_taken, name='time_taken'),
+
+    # lists...
     # /patient-list
     url(r'^patient_list/$', views.patient_list, name='patient_list'),
     # /appointment_list
     url(r'^appointment_list', views.appointment_list, name='appointment_list'),
     # /appointmentid/edit
     #url(r'^(?P<appointment>[0-9]+)/edit', views.EditAppointment, name='edit_appointment')
-    url(r'^appointment_list', views.appointment_list, name='appointment_list'),
-    # /edit_appointment
-    url(r'^edit_appointment//(?P<pk>[0-9]+)$', views.EditAppointment.as_view(), name='edit_appointment'),
-    # /edit_appointment/time_taken
-    url(r'^edit_appointment/time_taken', views.time_taken, name='time_taken'),
+
     # /admin/admin_log
-    url(r'^admin/admin_log', views.display_log, name='admin_log'),
+    url(r'^admin/admin_log/$', views.display_log, name='admin_log'),
+
+
     # /edit_medical_record
-    url(r'edit_medical_records/(?P<pk>[0-9]+)', views.EditMedicalRecordView.as_view(), name='edit_medical_record'),
+    url(r'edit_medical_records/(?P<pk>[0-9]+)/$', views.EditMedicalRecordView.as_view(), name='edit_medical_record'),
     # /create_medical_record
-    url(r'create_medical_records', views.CreateMedicalRecordView.as_view(), name='create_medical_record'),
+    url(r'create_medical_records/$', views.CreateMedicalRecordView.as_view(), name='create_medical_record'),
 ]
